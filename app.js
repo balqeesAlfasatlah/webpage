2fec3a3c43504121821731ddcd5cb3e6ed15e9c2
balqees22 <>
2021-05-27 13:13:37 +0000

    i made changed

---
+++ app.js
@@ -0,0 +1,16 @@
+var userName = prompt("Whats your name ?");
+alert("welocme back " + userName );
+
+var favColor = prompt("Whats your favourite color ?");
+alert(favColor  +"its nice color ");
+
+var age = prompt('What is your age?');
+if (age < 18){
+alert('you cant take this course');
+}
+else if(age >= 18){
+alert('you cant take this course');
+}
+else{
+  document.write("please enter your age");
+}
