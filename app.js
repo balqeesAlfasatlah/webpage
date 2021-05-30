ef73edd22a59551e42c3e52407545b47e4ff0321
balqees22 <>
2021-05-30 19:51:13 +0000

    changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var userName = prompt("Whats your name ?");
+/*var userName = prompt("Whats your name ?");
 alert("welocme back " + userName );
 
 var favColor = prompt("Whats your favourite color ?");
@@ -13,4 +13,13 @@ alert('you cant take this course');
 }
 else{
   document.write("please enter your age");
+}
+*/
+var userName = prompt("Please enter your name ");
+while(userName === ' ' || !isNaN(userName)){
+  userName = prompt("Please enter your name ");
+}
+var userNumber = prompt("Enter the number of the images do you want to see ");
+for(var i=0 ; i<userNumber; i++){
+  document.write('<img src= "https://i2.wp.com/www.annuair.ma/wp-content/uploads/2021/03/html-cours-gratuit.jpg?fit=590%2C320&ssl=1"/>');
 }
